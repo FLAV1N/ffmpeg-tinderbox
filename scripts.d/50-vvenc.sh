143049ef1c77a86a589ecee6981076fef8c2b999
FLAV1N <122433506+FLAV1N@users.noreply.github.com>
2024-04-30 04:22:34 +0700

    vvc again

--- scripts.d/50-vvenc.sh
+++ scripts.d/50-vvenc.sh
@@ -13,11 +13,18 @@ ffbuild_dockerbuild() {
 
     mkdir build && cd build
 
+    if [[ $TARGET == *arm64 ]]; then
+    fixarm64=(
+        -DVVENC_ENABLE_X86_SIMD=OFF
+    )
+    fi
+    
     cmake \
         -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
         -GNinja \
+        $fixarm64 \
         ..
     ninja -j$(nproc)
     ninja install
